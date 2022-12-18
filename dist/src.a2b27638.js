ca56ef806c4a5d087aa4251f5fd6062c903c8c15
Md_Shahid <mdshahidk0001@gmail.com>
2022-12-18 13:41:11 +0000

    Update 8 files

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -176,6 +176,48 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("./styles.css");
+var container = document.getElementById("container");
+var ball = document.getElementById("ball");
+var num = container.getBoundingClientRect();
+console.log(num);
+var a = 40; /// for moving left/right
+// let s=40;
+// let d=40;
+var w = 40;
+document.addEventListener("keydown", function (event) {
+  console.log("key pressed", event.keyCode);
+  if (event.keyCode == 87) {
+    //top W
+
+    if (w <= 0) {
+      return;
+    }
+    w -= 10;
+    ball.style.marginTop = w + "px";
+  } else if (event.keyCode == 83) {
+    // for S
+    if (w >= 500) {
+      return;
+    }
+    w += 10;
+    ball.style.marginTop = w + "px";
+  } else if (event.keyCode == 65) {
+    //for "A"
+    if (a <= 0) {
+      return;
+    }
+    a -= 10;
+    ball.style.marginLeft = a + "px";
+  } else if (event.keyCode == 68) {
+    //for "D"
+    if (a >= 1110) {
+      //    if(a>=710)
+      return;
+    }
+    a += 10;
+    ball.style.marginLeft = a + "px";
+  }
+});
 },{"./styles.css":"src/styles.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import "./styles.css";
+
+var container = document.getElementById("container");
+var ball = document.getElementById("ball");
+
+let num = container.getBoundingClientRect();
+
+console.log(num);
+
+let a = 40; /// for moving left/right
+// let s=40;
+// let d=40;
+let w = 40;
+document.addEventListener("keydown", function (event) {
+  console.log("key pressed", event.keyCode);
+
+  if (event.keyCode == 87) {
+    //top W
+
+    if (w <= 0) {
+      return;
+    }
+    w -= 10;
+    ball.style.marginTop = w + "px";
+  } else if (event.keyCode == 83) {
+    // for S
+    if (w >= 500) {
+      return;
+    }
+
+    w += 10;
+    ball.style.marginTop = w + "px";
+  } else if (event.keyCode == 65) {
+    //for "A"
+    if (a <= 0) {
+      return;
+    }
+    a -= 10;
+    ball.style.marginLeft = a + "px";
+  } else if (event.keyCode == 68) {
+    //for "D"
+    if (a >= 1110) {
+      //    if(a>=710)
+      return;
+    }
+    a += 10;
+    ball.style.marginLeft = a + "px";
+  }
+});
